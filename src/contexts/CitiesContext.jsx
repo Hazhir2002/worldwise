89a8072a6c2b3ef8b622ef3991863e23f8a949cc
Hazhir2002 <HazhirYousefi2002@gmail.com>
2024-02-27 22:42:31 +0330

    added lazy loading

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 import { createContext, useContext, useReducer } from "react";
-import { useEffect } from "react";
+import { useEffect, useCallback } from "react";
 
 const CitiesContext = createContext();
 
@@ -86,21 +86,24 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
-  async function getCity(id) {
-    if (Number(id) === currentCity.id) return;
+  const getCity = useCallback(
+    async function getCity(id) {
+      if (Number(id) === currentCity.id) return;
 
-    dispatch({ type: "loading" });
-    try {
-      const res = await fetch(`${BASE_URL}/cities/${id}`);
-      const data = await res.json();
-      dispatch({ type: "city/loaded", payload: data });
-    } catch {
-      dispatch({
-        type: "rejected",
-        payload: "There was an error loading data...",
-      });
-    }
-  }
+      dispatch({ type: "loading" });
+      try {
+        const res = await fetch(`${BASE_URL}/cities/${id}`);
+        const data = await res.json();
+        dispatch({ type: "city/loaded", payload: data });
+      } catch {
+        dispatch({
+          type: "rejected",
+          payload: "There was an error loading data...",
+        });
+      }
+    },
+    [currentCity.id]
+  );
 
   async function deleteCity(id) {
     try {
